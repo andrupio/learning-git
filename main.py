9daa71f7daae0e8e409db14c2bfa3554f0970615
andrupio <andrupio@gmail.com>
2021-02-22 18:02:17 +0100

    Fix sketch info

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ for key, value in shopping.items():
     print(f"Idę do", key.capitalize(),
           "kupuję tu następujące rzeczy:", str(value).title())
 print(f"W sumie kupuję", sum(len(i) for i in shopping.values()), "produktów")
+print("'Martwa papuga' to najlepszy skecz grupy Monty Pythona")
+print("'Hiszpańska inkwizycja' to najlepszy skecz grupy Monty Pythona")
